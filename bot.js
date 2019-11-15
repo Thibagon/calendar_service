2b5c3340287742a65ec8f0be0b51638af087ecb7
Thibagon <thibagon@gmail.com>
2019-11-15 18:17:14 +0100

    Added dependencies node-fetch

--- bot.js
+++ bot.js
@@ -1,4 +1,5 @@
 const Discord = require('discord.js');
+const fetch = require('node-fetch')
 const client = new Discord.Client();
 const auth = require('./auth.json');
 
@@ -7,9 +8,26 @@ client.on('ready', () => {
 });
 
 client.login(auth.token);
+const burgerEmoji = "🍔";
+const burgerReact = reaction => reaction.emoji.name === burgerEmoji;
 
 client.on('message', msg => {
-    if (msg.content === 'ping') {
-        msg.reply('pong');
+    if (msg.content.startsWith("!")) {
+        if(msg.content.substr(1) == "ping")
+            msg.reply('pong');
+        if(msg.content.substr(1)== "salle"){
+            fetch('http://51.254.133.142/sample/sample.json')
+            .then(res => res.json())
+            .then(json => {
+                for(let key in json){
+
+                }
+                return resultat
+            });
+        }
     }
+    msg.awaitReactions(burgerReact,{time:60000}).then(collected => {
+        if (collected.size > 0)
+            msg.reply("")
+    });
 });
